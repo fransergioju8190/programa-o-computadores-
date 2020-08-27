9a85981128f7b969f36143b4e536ee2a47a7cf06
fransergioju8190@gmail.com <fransergioju8190@gmail.com>
2020-08-26 22:03:54 -0300

    Exercicios

--- lista1 - exe7.js
+++ lista1 - exe7.js
@@ -1,1 +1,25 @@
-console.log(`Exercio 7`)
+chefe()
+
+function chefe(){
+    var vetor = []
+    exe7(vetor)// estamos passando o vetor como parametro - escopo global
+    console.log(vetor)
+    calculamedia(vetor)
+}
+function exe7(vet){// vet representa vetor -> o que fizemos com vet, será feito no vetor
+var numero =  Number(prompt(`Informe um número`))
+do{
+    // Coloca o numero em vet
+    vet.push(numero)
+    numero = Number(prompt(`Informe outro número, informe número negativo para encerrar`))
+}
+while(numero >= 0)
+}
+
+function calculamedia(vet){
+    var soma = 0
+    for(var i=0; i < vet.length; i++){
+        soma = soma + vet[i]
+    }
+    console.log(`A média é ${soma/vet.length}`)
+}
--- lista1 - exe9.js
+++ lista1 - exe9.js
@@ -1,1 +1,17 @@
-console.log(`Exercicio 9`)
+chefe()
+
+function chefe(){
+    var numero = Number(prompt(`Informe um número`))
+    
+    exe9(numero)
+}
+
+function exe9(num){ // Supondo num = 6
+     var soma = 0
+     for(var divisor = num; divisor >=1; divisor++){//Divisor começa em 1 e vai ate 6
+        if(num % divisor == 0){
+            soma =  soma + divisor
+        }
+    }    
+    console.log(`A soma dos divisores de ${num} é ${soma}`)
+}
